3f7450eae8f047bb68ab01655f79c37eb8799774
Perchouli <jp.chenyang@gmail.com>
2011-08-24 22:32:31 +0800

    Add profile form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from django.forms import ModelForm
+from onlinegalgame.accounts.models import UserProfile
 
 class RegisterForm(forms.Form):
     username = forms.CharField(max_length=16)
     password = forms.CharField(min_length=6, widget=forms.PasswordInput )
     email = forms.EmailField()
+
+class UserProfileForm(ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user', 'honor', 'career')
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import redirect, render_to_response
 from django.contrib.auth.decorators import login_required
 from django.template.context import RequestContext
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from onlinegalgame.accounts.models import UserProfile
-from onlinegalgame.accounts.forms import RegisterForm
+from onlinegalgame.accounts.forms import RegisterForm, UserProfileForm
 from onlinegalgame.role.models import Role, RoleEvent
 from onlinegalgame.story.models import UserStory, StoryEvent
 
@@ -31,28 +32,39 @@ def register(request):
             }
             return render_to_response('accounts/register.html', ctx , context_instance = RequestContext(request))
     else:
-	    return render_to_response('accounts/register.html',{'form' : RegisterForm() }, context_instance = RequestContext(request))
+	    return render_to_response('accounts/register.html',{'form' : RegisterForm()}, context_instance = RequestContext(request))
 
-@login_required
-def profile(request,uid):
-    # Unfinished
-    user = User.objects.get(id=uid)
-    try:
-        profile = user.get_profile()
-    except:
-        #profile = UserProfile.objects.create(user=user,qq='',msn='',gtalk='',website='')
-        profile = UserProfile(user=user,qq='',msn='',gtalk='',website='')
-        profile.save()
-    roles = Role.objects.filter(author=uid).filter(parent=0).order_by('-id')[0:5]
-    stories = UserStory.objects.filter(author=uid).order_by('sort')[0:5]
-    events = list(RoleEvent.objects.filter(user=uid)) + list(StoryEvent.objects.filter(user=uid))
-    
-    ctx = {
-        'user'  : user,
-        'roles' : roles,
-        'stories' : stories,
-        'profile' : profile,
-        'events'  : events
-    }
-    return render_to_response('accounts/profile.html', ctx ,context_instance = RequestContext(request))
+@csrf_exempt
+def profile(request, uid):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST)
+        if form.is_valid():
+            profile = UserProfile.objects.get(user=uid)
+            profile.qq = form.cleaned_data['qq']
+            profile.msn = form.cleaned_data['msn']
+            profile.gtalk = form.cleaned_data['gtalk']
+            profile.website = form.cleaned_data['website']
+            profile.save()
+            return redirect ('/accounts/profile/'+uid)
+    else:
+        user = User.objects.get(id=uid)
+        try:
+            profile = user.get_profile()
+        except:
+            #profile = UserProfile.objects.create(user=user,qq='',msn='',gtalk='',website='')
+            profile = UserProfile(user=user,qq='',msn='',gtalk='',website='')
+            profile.save()
+        roles = Role.objects.filter(author=uid).filter(parent=0).order_by('-id')[0:5]
+        stories = UserStory.objects.filter(author=uid).order_by('sort')[0:5]
+        events = list(RoleEvent.objects.filter(user=uid)) + list(StoryEvent.objects.filter(user=uid))
+        
+        ctx = {
+            'user'  : user,
+            'roles' : roles,
+            'stories' : stories,
+            'profile' : profile,
+            'events'  : events,
+            'form'    : UserProfileForm({'qq':profile.qq, 'msn':profile.msn, 'gtalk':profile.gtalk, 'website':profile.website})
+        }
+        return render_to_response('accounts/profile.html', ctx ,context_instance = RequestContext(request))
 
