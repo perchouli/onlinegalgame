42845569b382eb3ff50e6f41f34ddbb520a0de89
perchouli <jp.chenyang@gmail.com>
2011-10-08 14:12:00 +0800

    Add show story page

--- mobile/views.py
+++ mobile/views.py
@@ -6,6 +6,10 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 
+from onlinegalgame.story.models import UserStory
+from onlinegalgame.role.models import Role, LinkRole
+from onlinegalgame.fileupload.models import StoryUpload
+from onlinegalgame.settings import PROJECT_PATH
 
 def index(request):
     
@@ -13,5 +17,17 @@ def index(request):
     return render_to_response('mobile/index.html', ctx, context_instance = RequestContext(request))
 
 def show_story(request, story_id):
-    ctx = {}
+    story = UserStory.objects.get(id=story_id)
+    command = story.process
+    
+    role_list = list(Role.objects.filter(author=story.author.id))
+    link_role_list = LinkRole.objects.filter(author=story.author.id)
+    #SecurityHash End
+    for role in link_role_list:
+        role_list.append(role.linkrole)
+    ctx = {
+        'story'     : story,
+        'command'   : command,
+        'role_list' : role_list,
+    }
     return render_to_response('mobile/show.html', ctx, context_instance = RequestContext(request))
