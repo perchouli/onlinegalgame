f180a45883bd091102f046815ff2daff4072281c
perchouli <jp.chenyang@gmail.com>
2012-01-27 11:39:07 +0800

    Deploy in nodejs

---
+++ app.js
@@ -0,0 +1,55 @@
+/*
+http.createServer(function(req, res) {
+	res.writeHead(200, {'Content-Type' : 'text/html'});
+	res.end('<h2>Test</h2>');
+}).listen(1337, "127.0.0.1");
+
+*/
+var mongoskin = require('mongoskin');
+var express = require('express');
+var less = require('less')
+var parser = new(less.Parser);
+var app = express.createServer();
+//var mongourl = "perchouli:Forelord12@kanoya-perchouli-data-0.dotcloud.com:20661/kanoya?auto_reconnect";
+var mongourl = "localhost:27017/olgg";
+var db = mongoskin.db(mongourl);
+//db.bind('items');
+
+app.configure(function(){
+    //Static
+    app.use(express.compiler({ src: __dirname + '/public/css', enable: ['less']}));
+    app.use(express.static(__dirname + '/public'));
+    
+    //Middleware
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    
+    //Template
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'html');
+    app.register(".html", require("jqtpl").express);
+
+});
+app.configure('production', function(){
+    var oneYear = 31557600000;
+    app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+    app.use(express.errorHandler());
+});
+
+//Routing
+app.get('/', function(req, res){
+    db.collection('items').find().toArray(function(err, items){
+        res.render('item', { pageTitle: 'Items', 'shopItems' :JSON.stringify(items) });
+    })
+});
+
+app.get('/role/create', function(req, res){
+    res.render('role/create', { pageTitle: 'Items' });
+});
+
+app.post('/', function(req, res){
+    console.log(req);
+});
+app.listen(3000)
+
