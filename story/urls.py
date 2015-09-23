7e64451952994a9ad9e8d3bad1fa9bf2327bd9cf
Perchouli <jp.chenyang@gmail.com>
2015-09-23 10:38:37 +0800

    Simple story list

--- story/urls.py
+++ story/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import include, url
 from .views import *
 
 urlpatterns = [
-    url(r'^create/$', story_create_or_edit, name='story_create'),
+    # url(r'^create/$', story_create_or_edit, name='story_create'),
+    url(r'^$', story_list, name='story_list'),
     url(r'^(?P<story_id>\d+)/$', story_play, name='story_play'),
     url(r'^edit/(?P<story_id>\d+)/$', story_create_or_edit, name='story_edit'),
 ]
